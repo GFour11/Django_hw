39899b09c30b8da903cb8164bd0d19f56064cfe3
ulche <ulchegor1@gmaol.com>
2023-07-15 15:58:15 +0300

    1

---
+++ quotes_site/quote/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+from django.db.models import CharField, ForeignKey
+from django.contrib.postgres.fields import ArrayField
+
+
+class Author(models.Model):
+    fullname = CharField(max_length=100)
+    born_date = CharField()
+    born_location = CharField()
+    description = CharField()
+
+
+class Quotes(models.Model):
+    tags = ArrayField(models.CharField(max_length=50), blank=True, null=True)
+    author = ForeignKey(Author, on_delete=models.CASCADE)
+    quotes = CharField()
+
+
